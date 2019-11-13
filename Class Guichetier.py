9f8b3227f7491b9925b676c85a6bd530a2f99d5c
EmmaDjeufa <57154645+EmmaDjeufa@users.noreply.github.com>
2019-11-13 22:42:23 +0100

    Class Guichetier.py

--- Class Guichetier.py
+++ Class Guichetier.py
@@ -1,32 +1,39 @@
-# coding:utf-8
+#codint:utf-8
 
-class Gestionnaire:
-
-    def __init__(self, emprunt, compte):
-        self.emprunt = emprunt
-        self.compte = compte
-
-    def ajout(self, compte):
-        print("le compte ajouté est {}".format(self.compte))
-
-    def modifier(self, compte):
-        print("le compte modifier est {}".format(self.compte))
-
-    def suppression(self, compte):
-        print("le compte {} est supprimé avec succès".format(self.compte))
-
-    def enregistrer(self, emprunt):
-        if self.emprunt == 0:
-            print("Le client n'a pas de dêtte")
+class guichetier: 
+      def __init__(self, id, MotDePasse):#methode permettant de connaitre le montant danscompte
+        self.id = id
+        self.MotDePasse = MotDePasse
+        self.solde = 50000
+      def depot(self):#methode pour le depot de la somme
+        montant = input("entrer le montant du depot...\n")
+        montant = int(montant)
+        self.solde += montant
+        print("vous avez effectuer un depot de ", montant)
+        print("nouveau solde", self.solde)
+      def retret (self):#methode pour retirer
+        montants = input("combien vouliez vous retirer\n")
+        montants = int(montants)
+        if montants <= self.solde:
+           self.solde = self.solde-montants
+           print("vous avez effectuer un retrait de ", montants)
+           print("nouveau solde", self.solde)
         else:
-            print("Le client a un emprunt")
+           print("solde insuffisant")        
+      def virement(self):
+        compt = 10000
+        som = input("combien vouliez vous virer\n")
+        som = int(som)
+        if som <= self.solde:
+            compt += som
+            self.solde = self.solde - som
+            print("vous avez effectuer un virement de", som)
+            print("votre nouveau solde est ", self.solde)
+        else:
+            print("desole solde insuffisant")
+ab = guichetier("adan", "azer123")
+ab.depot()
+ab.retret()
+ab.virement()
 
-
-g1 = Gestionnaire(10000, 50000)
-g1.ajout("le premier compte est:")
-g1.enregistrer("emprunt enregistrer")
-
-g2 = Gestionnaire(0, 100000)
-g2.modifier("compte modifier")
-g2.enregistrer("emprunt enregistrer")
-g2.suppression("supprimer compte")
+os.system("pause")
